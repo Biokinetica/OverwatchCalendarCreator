03c5f1881d6a5bf3ee66359c2ec7d6a7e03bbf3a
Brenton Poke <akioserano@hotmail.com>
2017-05-03 16:55:48 -0400

    updates

--- src/main/java/App/Runner.java
+++ src/main/java/App/Runner.java
@@ -1,9 +1,72 @@
 package App;
-
+import ch.wisv.toornament.ToornamentClient;
+import ch.wisv.toornament.concepts.Disciplines;
+import ch.wisv.toornament.concepts.Tournaments;
+import ch.wisv.toornament.model.Discipline;
+import ch.wisv.toornament.model.DisciplineDetails;
+import ch.wisv.toornament.model.Tournament;
+import ch.wisv.toornament.model.request.ApiTokenRequest;
+import ch.wisv.toornament.model.request.TournamentRequest;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 public class Runner {
     /**
      * @param args the command line arguments
      */
+ 
+
+    /*    public Runner() throws FileNotFoundException
+    {
+    Scanner linReader = new Scanner(new File("dataFile.txt"));
+    
+    
+    API = linReader.nextLine();
+    id = linReader.nextLine();
+    secret = linReader.nextLine();
+    
+    linReader.close();
+    
+    }*/
+
+
     public static void main(String[] args) {
+String API = "";
+String id = "";
+String secret = "";
+    try (Scanner linReader = new Scanner(new File("C:\\Users\\Brenton\\Desktop\\dataFile.txt"))) {
+        API = linReader.nextLine();
+        id = linReader.nextLine();
+        secret = linReader.nextLine();
+    } catch (FileNotFoundException ex) {
+        Logger.getLogger(Runner.class.getName()).log(Level.SEVERE, null, ex);
+    }
+        ToornamentClient Client = new ToornamentClient(API,id,secret);
+        Client.authorize();
+        
+            
+        try {
+           List<Discipline> details = Client.disciplines().getDisciplines();
+           
+           Iterator<Discipline> iterator = details.iterator();
+	while (iterator.hasNext()) {
+		System.out.println(iterator.next());
+	}
+        } catch (Exception e) {
+            System.out.println(e.getCause());
+        }
+        
+        
+    
+        
+        
     }
 }
